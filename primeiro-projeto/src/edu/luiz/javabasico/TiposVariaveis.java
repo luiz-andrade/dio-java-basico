b7546f59868d45b0b5dd1499bcd25b643818c503
Luiz Andrade Gonçalves Melo <luizz_andrade@hotmail.com>
2024-08-13 21:57:17 -0400

    feat: adicionado novos tipo

--- primeiro-projeto/src/edu/luiz/javabasico/TiposVariaveis.java
+++ primeiro-projeto/src/edu/luiz/javabasico/TiposVariaveis.java
@@ -2,4 +2,15 @@ package edu.luiz.javabasico;
 
 public class TiposVariaveis {
 
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;//adicionado cast para não apresentar erro de compilação
+
+        //Constante valor que não poderão mudar
+        //Deve ser escrito em CAIXA ALTA e com "final"
+        final double VALOR_DE_PI = 3.14;
+    }
 }
